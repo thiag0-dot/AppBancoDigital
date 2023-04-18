7bd08882ad88d5741ed28c5efb818648cfab5bb1
thiag0-dot <thiagodeginofilho@gmail.com>
2023-04-18 15:41:13 -0300

    Adicionar arquivos de projeto.

---
+++ AppBancoDigital/AppBancoDigital/Model/Correntista.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppBancoDigital.Model
+{
+    public class Correntista
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime Data_Nasc { get; set; }
+        public int Cpf { get; set; }
+        public string Senha { get; set; }
+    }
+}
