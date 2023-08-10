34e0acdea005dedf2c722413abef27d682410def
thiag0-dot <thiagodeginofilho@gmail.com>
2023-08-10 15:54:52 -0300

    Começando a criar um menu hamburguer

---
+++ AppBancoDigital/AppBancoDigital/View/Menu.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View
+{
+    
+    [DesignTimeVisible(false)]
+    public partial class Menu : MasterDetailPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Home)));
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Home)));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            try
+            {
+                var operation = await DisplayAlert("Atenção!", "Você vai sair de sua conta", "Sim", "Nao");
+                string resumo = operation.ToString();
+                if (resumo == "Nao")
+                {
+                    return;
+                }
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Home)));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+    }
+}
